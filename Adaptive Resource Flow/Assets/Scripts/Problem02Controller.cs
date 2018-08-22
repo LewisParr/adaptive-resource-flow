7de544d49257c5cf18d79b1c8a643bd241a95c3d
LewisParr <lewisparr@gmail.com>
2018-08-22 16:16:24 +0100

    Delivery Assignment and Supply Visualisation

--- Adaptive Resource Flow/Assets/Scripts/Problem02Controller.cs
+++ Adaptive Resource Flow/Assets/Scripts/Problem02Controller.cs
@@ -16,6 +16,12 @@ public class Problem02Controller : MonoBehaviour
 	private float[,] minCost;
 	private IDictionary<Vector2, int[]> minCostPath;
 	private float[] rate;
+	private float[,] flow;
+
+	List<int> source;
+	List<int> sink;
+	List<float> cost;
+	float[] remain;
 
 	private int frameCount;
 
@@ -24,6 +30,7 @@ public class Problem02Controller : MonoBehaviour
 		// Initialise
 		minCost = new float[nNodes, nNodes];
 		minCostPath = new Dictionary<Vector2, int[]>();
+		flow = new float[nNodes, nNodes];
 		frameCount = 0;
 
 		// Generate random nodes
@@ -43,15 +50,30 @@ public class Problem02Controller : MonoBehaviour
 
 		// Generate random sources and sinks
 		rate = RandomRates(totalProduction, minProduction, maxProduction);
+
+		// Find each delivery option
+		source = new List<int>();
+		sink = new List<int>();
+		cost = new List<float>();
+		remain = new float[nNodes];
+		for (int i = 0; i < nNodes; i++) remain[i] = rate[i];
+		for (int a = 0; a < nNodes; a++) if (rate[a] > 0)
+		{
+			// For each source, ...
+			for (int b = 0; b < nNodes; b++) if (rate[b] < 0)
+			{
+				// ... and each sink
+				source.Add(a); // Record source index
+				sink.Add(b); // Record sink index
+				cost.Add(minCost[a, b]); // Record cost of delivery
+			}
+		}
 	}
 
 	void Update()
 	{
 		frameCount++;
-		if (frameCount % 100 == 0)
-		{
-
-		}
+		if (frameCount % 100 == 0) AssignDelivery();
 	}
 
 	void OnDrawGizmos()
@@ -63,6 +85,22 @@ public class Problem02Controller : MonoBehaviour
 			else if (rate[n] < 0) Gizmos.color = Color.red;
 			else Gizmos.color = Color.green;
 			Gizmos.DrawSphere(new Vector3(nodeX[n], 0, nodeY[n]), 0.2f);
+		}
+
+		// Draw flows
+		Gizmos.color = Color.white;
+		for (int a = 0; a < nNodes; a++)
+		{
+			for (int b = 0; b < nNodes; b++)
+			{
+				float amount = flow[a, b];
+				if (amount > 0)
+				{
+					Vector3 pointA = new Vector3(nodeX[a], 0, nodeY[a]);
+					Vector3 pointB = new Vector3(nodeX[b], 0, nodeY[b]);
+					Gizmos.DrawLine(pointA, pointB);
+				}
+			}
 		}
 	}
 
@@ -137,5 +175,98 @@ public class Problem02Controller : MonoBehaviour
 
 		// Return rates
 		return r;
+	}
+
+	private void AssignDelivery()
+	{
+		int nOptions = source.Count;
+		Debug.Log(nOptions + " delivery options remaining.");
+		if (nOptions > 0)
+		{
+			// Find the cheapest delivery option
+			float minValue = float.MaxValue;
+			int minIndex = -1;
+			for (int i = 0; i < nOptions; i++) if (cost[i] < minValue)
+			{
+				minValue = cost[i];
+				minIndex = i;
+			}
+			Debug.Log("Minimum cost delivery is " + minValue + " at index " + minIndex);
+			int _source = source[minIndex]; // Node index of source of cheapest delivery option
+			int _sink = sink[minIndex]; // Node index of sink of cheapest delivery option
+			float available = remain[_source];
+			float required = remain[_sink];
+			Vector2 key = new Vector2(_source, _sink);
+			int[] deliveryPath = minCostPath[key];
+			Debug.Log(required+" is required; "+available+" is available.");
+			if (Mathf.Abs(required) <= Mathf.Abs(available))
+			{
+				remain[_source] += required;
+				remain[_sink] -= required;
+				for (int i = 0; i < deliveryPath.Length - 1; i++)
+				{
+					int a = deliveryPath[i];
+					int b = deliveryPath[i + 1];
+					flow[a, b] -= required;
+				}
+
+				Debug.Log("Source can satisfy sink.");
+				Debug.Log(required+" of "+rate[_source]+" was sourced, "+remain[_source]+" remains.");
+				Debug.Log(required+" of "+rate[_sink]+" was sunk, "+remain[_sink]+" remains.");
+
+				// Remove sink
+				List<int> toRemove = new List<int>();
+				for (int i = 0; i < nOptions; i++) if (sink[i] == _sink) toRemove.Add(i);
+				for (int a = 0; a < toRemove.Count; a++)
+				{
+					int i = toRemove[a];
+					for (int b = 0; b < a; b++) if (toRemove[b] < toRemove[a]) i--;
+					source.RemoveAt(i);
+					sink.RemoveAt(i);
+					cost.RemoveAt(i);
+				}
+
+				if (remain[_source] == 0)
+				{
+					// Remove source
+					toRemove.Clear();
+					for (int i = 0; i < nOptions; i++) if (source[i] == _source) toRemove.Add(i);
+					for (int a = 0; a < toRemove.Count; a++)
+					{
+						int i = toRemove[a];
+						for (int b = 0; b < a; b++) if (toRemove[b] < toRemove[a]) i--;
+						source.RemoveAt(i);
+						sink.RemoveAt(i);
+						cost.RemoveAt(i);
+					}
+				}
+			}
+			else
+			{
+				remain[_source] -= available;
+				remain[_sink] += available;
+				for (int i = 0; i < deliveryPath.Length - 1; i++)
+				{
+					int a = deliveryPath[i];
+					int b = deliveryPath[i + 1];
+					flow[a, b] += available;
+				}
+
+				Debug.Log(available+" of "+rate[_source]+" was sourced, "+remain[_source]+" remains.");
+				Debug.Log(available+" of "+rate[_sink]+" was sunk, "+remain[_sink]+" remains.");
+
+				// Remove source
+				List<int> toRemove = new List<int>();
+				for (int i = 0; i < nOptions; i++) if (source[i] == _source) toRemove.Add(i);
+				for (int a = 0; a < toRemove.Count; a++)
+				{
+					int i = toRemove[a];
+					for (int b = 0; b < a; b++) if (toRemove[b] < toRemove[a]) i--;
+					source.RemoveAt(i);
+					sink.RemoveAt(i);
+					cost.RemoveAt(i);
+				}
+			}
+		}
 	}
 }
