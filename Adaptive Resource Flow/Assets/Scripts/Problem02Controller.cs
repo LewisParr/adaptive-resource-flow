b37fdee7ae937cb5903e03858f0e83b3bc6f3df6
LewisParr <lewisparr@gmail.com>
2018-08-22 13:52:59 +0100

    Empty Problem 2

---
+++ Adaptive Resource Flow/Assets/Scripts/Problem02Controller.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Problem02Controller : MonoBehaviour
+{
+	public int nNodes = 12;
+	public int totalProduction = 5;
+	public float distanceExponent = 1.25f;
+}
