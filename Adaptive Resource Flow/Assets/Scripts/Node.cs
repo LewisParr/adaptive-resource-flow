82018181bfc2f8e59ffaede3aa75080f09fcec8f
LewisParr <lewisparr@gmail.com>
2018-08-23 15:44:34 +0100

    Node Creation

---
+++ Adaptive Resource Flow/Assets/Scripts/Node.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+	public Vector3 pos;
+
+	public Node(Vector3 pos)
+	{
+		this.pos = pos;
+	}
+}
---
+++ Adaptive Resource Flow/Assets/Scripts/Problem04Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Problem04Controller : MonoBehaviour
+{
+	public int numNodes = 20;
+
+	private List<Node> node;
+
+	void OnEnable()
+	{
+
+	}
+
+	private void GenerateNodes()
+	{
+		float x = Random.value * numNodes;
+		float y = Random.value * numNodes;
+
+		Vector3 pos = new Vector3(x, 0, y);
+
+		node.Add(new Node(pos));
+	}
+}
