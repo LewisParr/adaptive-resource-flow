63110b23688803f2bfde2e1a793b8d4296dfd5e2
Lewis C. Parr <lewisparr@gmail.com>
2018-09-13 20:32:40 +0100

    Generate System Properties

--- Adaptive Resource Flow/Assets/Scripts/Problem06Controller.cs
+++ Adaptive Resource Flow/Assets/Scripts/Problem06Controller.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Problem06Controller : MonoBehaviour
 {
     [Range(3, 24)]
-    public int numNodes = 20;
+    public int numSys = 20;
     [Range(0.1f, 0.5f)]
     public float productionProb = 0.25f;
     [Range(1, 10)]
@@ -19,4 +19,55 @@ public class Problem06Controller : MonoBehaviour
     private int frameCounter;
     private bool allNodesGenerated;
     private bool flowsCalculated;
+
+    private void OnEnable()
+    {
+        frameCounter = 0;
+        allNodesGenerated = false;
+        flowsCalculated = false;
+    }
+
+    private void Update()
+    {
+        frameCounter++;
+
+        if (!allNodesGenerated)
+        {
+            if (frameCounter % 10 == 0)
+            {
+                GenerateSystem();
+            }
+        }
+    }
+
+    private void GenerateSystem()
+    {
+        if (all == null) all = new List<AstroObject>();
+
+        int nAll = all.Count;
+
+        // Position
+        float x = Random.value * numSys;
+        float y = Random.value * numSys;
+        Vector3 pos = new Vector3(x, 0, y);
+
+        // Internal capacities
+        float[] intcap = new float[2];
+        intcap[0] = 4; intcap[1] = 4;
+
+        // Internal taxes
+        float[] inttax = new float[2];
+        inttax[0] = 0.05f; inttax[1] = 0.05f;
+
+        // Import/Export capacities
+        float[] imexcap = new float[2];
+        imexcap[0] = 4; imexcap[1] = 4;
+
+        // Import/Export taxes
+        float[] imextax = new float[2];
+        imextax[0] = 0.10f; imextax[1] = 0.10f;
+
+        // Throughflow tax
+        float thrutax = 0.10f;
+    }
 }
