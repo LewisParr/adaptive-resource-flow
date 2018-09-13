f4c996db4edde01c037ed3fc940baee1dd67150d
Lewis C. Parr <lewisparr@gmail.com>
2018-09-13 22:45:43 +0100

    AstroEdge and SystemEdge

---
+++ Adaptive Resource Flow/Assets/Scripts/AstroEdge.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstroEdge
+{
+    private AstroObject target; public AstroObject Target { get { return Target; } }
+
+    public AstroEdge(AstroObject target)
+    {
+        this.target = target;
+    }
+}
+
+public class SystemEdge : AstroEdge
+{
+    private float distance; public float Distance { get { return distance; } }
+
+    public SystemEdge(AstroObject target, float distance) : base(target)
+    {
+        this.distance = distance;
+    }
+}
