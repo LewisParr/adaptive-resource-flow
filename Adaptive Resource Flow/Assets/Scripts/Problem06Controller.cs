c10d9185e112b1238d16ac7e6ce77f35907d5725
Lewis C. Parr <lewisparr@gmail.com>
2018-09-13 22:38:59 +0100

    Generate Body and Facility Properties

--- Adaptive Resource Flow/Assets/Scripts/Problem06Controller.cs
+++ Adaptive Resource Flow/Assets/Scripts/Problem06Controller.cs
@@ -69,5 +69,70 @@ public class Problem06Controller : MonoBehaviour
 
         // Throughflow tax
         float thrutax = 0.10f;
+
+        // Instantiate system
+        SystemObject S = new SystemObject(pos, intcap, inttax, imexcap, imextax, thrutax);
+
+        // Number of planetary bodies
+        int numBody = Mathf.FloorToInt(Random.value * 8);
+
+        for (int b = 0; b < numBody; b++)
+        {
+            // Position
+            float _x = Random.value;
+            float _y = Random.value;
+            Vector3 _pos = pos + new Vector3(_x, 0, _y);
+
+            // Internal capacities
+            float[] _intcap = new float[2];
+            _intcap[0] = 2; _intcap[1] = 2;
+
+            // Internal taxes
+            float[] _inttax = new float[2];
+            _inttax[0] = 0.05f; _inttax[1] = 0.05f;
+
+            // Import/Export capacities
+            float[] _imexcap = new float[2];
+            _imexcap[0] = 2; _imexcap[1] = 2;
+
+            // Import/Export taxes
+            float[] _imextax = new float[2];
+            _imextax[0] = 0.10f; _imextax[1] = 0.10f;
+
+            // Instantiate body
+            BodyObject B = new BodyObject(_pos, _intcap, _inttax, _imexcap, _imextax);
+
+            // Number of facilities
+            int numFacility = Mathf.FloorToInt(Random.value * 16);
+
+            for (int f = 0; f < numFacility; f++)
+            {
+                // Position
+                float __x = Random.value / 2f;
+                float __y = Random.value / 2f;
+                Vector3 __pos = _pos + new Vector3(__x, 0, __y);
+
+                // Production
+                float[] prod = new float[numResources];
+                for (int r = 0; r < numResources; r++)
+                {
+                    if (Random.value < productionProb)
+                    {
+                        prod[r] = Random.value - 0.5f;
+                    }
+                }
+
+                // Import/Export capacity
+                float[] __imexcap = new float[2];
+                __imexcap[0] = 1; __imexcap[1] = 1;
+
+                // Import/Export taxes
+                float[] __imextax = new float[2];
+                __imextax[0] = 0.10f; __imextax[1] = 0.10f;
+
+                // Instantiate facility
+                FacilityObject F = new FacilityObject(__pos, prod, __imexcap, __imextax);
+            }
+        }
     }
 }
